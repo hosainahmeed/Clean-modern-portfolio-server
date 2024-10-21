051706670b0f3bb0ce9d3b4ab6ba5ff604668218
hosainahmed <hosainahmed534745@gmail.com>
2024-10-22 04:06:48 +0600

    vercel deployment

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
+app.use(cookieParser());
+// require('crypto').randomBytes(64).toString('hex');
 // MongoDB URI
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
@@ -20,17 +28,55 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.cookie.token;
+    if (!token) {
+      res.status(401).send({ message: "you dont have permisson for that" });
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_JWT, (err, decoded) => {
+      if (err) {
+        res.status(404).send({ message: "you dont have permisson for that" });
+      }
+      req.user = decoded;
+      next();
+    });
+  } catch (error) {
+    res.status(403).send({ message: "you dont have permisson for that" });
+  }
+};
+
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("Connected to MongoDB!");
 
     const allSkillsCollection = client.db("profileDb").collection("Skills");
+    const aboutCollection = client.db("profileDb").collection("aboutMe");
 
     // Home route
     app.get("/", (req, res) => {
       res.send("Home page");
     });
+
+    app.post("/jwt", async (req, res) => {
+      try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, {
+          expiresIn: "1d",
+        });
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+          })
+          .send({ success: true, message: "JWT token issued" });
+      } catch (error) {
+        res.status(401).send({ message: "you dont have permisson for that" });
+      }
+    });
+
+    // About route
 
     // Get all skills
     app.get("/skill", async (req, res) => {
@@ -53,26 +99,63 @@ async function run() {
         res.status(500).send({ message: "Failed to add skill" });
       }
     });
-    
 
     app.delete("/skill/:id", async (req, res) => {
       try {
         const skillId = req.params.id;
-        console.log(skillId);
-        const result = await allSkillsCollection.deleteOne({ _id: new ObjectId(skillId) });
+        const result = await allSkillsCollection.deleteOne({
+          _id: new ObjectId(skillId),
+        });
         res.send(result);
       } catch (error) {
         console.error("Error deleting skill:", error);
         res.status(500).send({ message: "Failed to delete skill" });
       }
     });
-    // Gracefully handle application shutdown
-    process.on("SIGINT", async () => {
-      await client.close();
-      console.log("MongoDB connection closed.");
-      process.exit(0);
+
+    app.put("/skill/:id", async (req, res) => {
+      try {
+        const skillId = req.params.id;
+        const data = req.body;
+        const id = { _id: new ObjectId(skillId) };
+        const options = { upsert: true };
+        const updateSkill = {
+          $set: {
+            name: data.name,
+            proficiency: data.proficiency,
+            experience_years: parseInt(data.experience_years),
+            image: data.image,
+            category: data.selectedSkillType,
+            description: data.description,
+          },
+        };
+        const result = await allSkillsCollection.updateOne(
+          id,
+          updateSkill,
+          options
+        );
+        res.send(result);
+      } catch (error) {
+        console.error("Error deleting skill:", error);
+        res.status(500).send({ message: "Failed to delete skill" });
+      }
     });
 
+    app.get("/about", async (req, res) => {
+      try {
+        const result = await aboutCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Error retrieving data", error });
+      }
+    });
+
+    // Gracefully handle application shutdown
+    // process.on("SIGINT", async () => {
+    //   await client.close();
+    //   console.log("MongoDB connection closed.");
+    //   process.exit(0);
+    // });
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
